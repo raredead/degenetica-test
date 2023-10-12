c5049c19d657baedb54bb0660c951ac4533c1fa0
raredead <smahelzed@gmail.com>
2023-10-12 18:14:25 +0700

    Добавление компонента header и переход с css на scss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
-
+import HeaderProcect from './components/header/HeaderProcect.vue';
 </script>
 
 <template>
-  
+  <div class="container">
+    <HeaderProcect />
+  </div>
 </template>
