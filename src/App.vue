08299d1a998db39a7e63e7bb9dc56121755caff3
raredead <smahelzed@gmail.com>
2023-10-12 20:51:14 +0700

    Подключение к основному проекту

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script setup>
 import HeaderProcect from './components/header/HeaderProcect.vue';
+import AsideCategoties from './components/Aside/AsideCategories.vue';
+import AsidePrice from './components/Aside/AsidePrice.vue';
+import AsideBrand from './components/Aside/AsideBrand.vue';
+import AsideSize from './components/Aside/AsideSize.vue';
 </script>
 
 <template>
   <div class="container">
     <HeaderProcect />
+    <main>
+      <div class="assides__block">
+        <AsideCategoties />
+        <AsidePrice />
+        <AsideBrand />
+        <AsideSize />
+      </div>
+      <div class="content__block"></div>
+    </main>
   </div>
 </template>
