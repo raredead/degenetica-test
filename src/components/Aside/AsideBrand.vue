dc0d590ea97f8ae2f5301a28108af23dce16517c
raredead <smahelzed@gmail.com>
2023-10-13 01:54:54 +0700

    Функционал удаления

--- src/components/Aside/AsideBrand.vue
+++ src/components/Aside/AsideBrand.vue
@@ -1,4 +1,5 @@
 <script setup>
+
 import { ref, computed } from 'vue';
 
 const titleBrand = 'Бренд'
@@ -27,8 +28,37 @@ const queryAtributs = computed(() => {
         a = a.filter((atribut) => atribut.title.indexOf(seacrh) !== -1)
     }
 
+    if(a.length === 0) {
+        return [
+            {
+                id: 0,
+                title: 'По вашему запросу ничего не найдено',
+            },
+        ]
+    }
+
     return a;
 })
+
+// Кнопка удаления
+document.addEventListener('DOMContentLoaded', () => {
+    const searchInput = document.querySelector('.aside-brand__search');
+    const searchRemoveButton = document.querySelector('.search-brand__remove');
+
+    searchInput.addEventListener('input', () => {
+        if (searchInput.value.length > 0) {
+            searchRemoveButton.classList.add('--active');
+        } else {
+            searchRemoveButton.classList.remove('--active');
+        }
+    });
+
+    searchRemoveButton.addEventListener('click', () => {
+        searchInput.value = '';
+        searchRemoveButton.classList.remove('--active');
+        searchRemoveButton.classList.add('--hidden');
+    });
+});
 </script>
 
 <template>
@@ -37,18 +67,28 @@ const queryAtributs = computed(() => {
             <div class="header-aside__title">{{ titleBrand }}</div>
             <button class="header-aside__btn">Очистить</button>
         </div>
-        <input 
-        type="text" 
-        class="aside-brand__search"
-        placeholder="Поиск"
-        v-model="query"
-        >
-        <div class="aside-brand__atributs atributs-brand">
+        <div class="wrap">
+            <input 
+                type="text" 
+                class="aside-brand__search"
+                placeholder="Поиск"
+                v-model="query"
+            >
+            <button class="search-brand__remove">
+                <svg class="remove" width="18" height="18" viewBox="0 0 18 18" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M17.3334 9.00001C17.3334 13.6024 13.6025 17.3333 9.00008 17.3333C4.39771 17.3333 0.666748 13.6024 0.666748 9.00001C0.666748 4.39764 4.39771 0.666679 9.00008 0.666679C13.6025 0.666679 17.3334 4.39764 17.3334 9.00001ZM12.0893 7.08927C12.4148 6.76383 12.4148 6.23619 12.0893 5.91076C11.7639 5.58532 11.2363 5.58532 10.9108 5.91076L9.00008 7.8215L7.08934 5.91076C6.7639 5.58532 6.23626 5.58532 5.91083 5.91076C5.58539 6.23619 5.58539 6.76383 5.91083 7.08927L7.82157 9.00001L5.91083 10.9108C5.58539 11.2362 5.58539 11.7638 5.91083 12.0893C6.23626 12.4147 6.7639 12.4147 7.08934 12.0893L9.00008 10.1785L10.9108 12.0893C11.2363 12.4147 11.7639 12.4147 12.0893 12.0893C12.4148 11.7638 12.4148 11.2362 12.0893 10.9108L10.1786 9.00001L12.0893 7.08927Z" />
+                </svg>
+            </button>
+        </div>
+        <div class="aside-brand__atributs atributs-brand" v-if="queryAtributs !== 0">
             <div class="atributs-brand__item" v-for="item in queryAtributs" :key="item.id">
                 <label>
                     <input 
                     type="checkbox" 
                     class="check"
+                    v-if="item.title !== 'По вашему запросу ничего не найдено'"
                     >
                     {{ item.title }}
                 </label> 
@@ -68,6 +108,7 @@ const queryAtributs = computed(() => {
         background-position: 3px 50%;
         outline: none;
         border: 1px solid var(--color-border);
+        border-radius: 4px;
 
         font-family: 'PT Sans';
         font-weight: 400;
@@ -133,5 +174,27 @@ const queryAtributs = computed(() => {
 
 .check {
     margin-right: 12px;
+}
+
+.wrap{
+    position: relative;
+}
+.search-brand{
+&__remove {
+        position: absolute;
+        top: 25px;
+        right: 5px;
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+        display: none;
+    }
+
+    &__remove.--active {
+        display: block;
+    }
+}
+.remove {
+    fill: var(--color-icons);
 }
 </style>
--- src/components/global/UbuttonNone.vue
+++ src/components/global/UbuttonNone.vue
@@ -1,28 +1,39 @@
 <template>
     <div class="btn-none">
-        <a href="" class="btn-none__message"><slot></slot></a>
+        <a href="" class="btn-none__message">
+            <slot></slot>
+        </a>
     </div>
 </template>
 
 <style lang="scss" scoped>
-.btn-none{
+.btn-none {
     width: 300px;
     display: flex;
     align-items: center;
     justify-content: center;
 
-    margin-top: 30px;
+    margin-top: 35px;
     border: 1px solid var(--color-font-second);
     padding: 12px 16px;
     border-radius: 4px;
     cursor: pointer;
     transition: .3s ease-in;
 
+    @media (max-width: 1650px) {
+        padding: 7px 15px;
+        width: 130px;
+    }
+
     &__message {
         font-family: 'PT Sans';
         font-size: 14px;
         font-weight: bold;
         color: var(--color-font-second);
+
+        @media (max-width: 500px) {
+            font-size: 12px;
+        }
     }
 }
 
